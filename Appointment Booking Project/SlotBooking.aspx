ffb490e517624db123c59c7a20525a10164cfc55
mridulthakur009 <mridul.thakur.007@gmail.com>
2024-02-21 13:28:21 +0530

    updated code

---
+++ Appointment Booking Project/SlotBooking.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SlotBooking.aspx.cs" Inherits="WebApplication1.slot_booking.SlotBooking" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Slot Booking</title>
+</head>
+<body>
+    <center>
+        <form id="form1" runat="server">
+            <div>
+                <asp:Label runat="server" Text="SLOT BOOKING SYSTEM"
+                    Font-Bold="True" Font-Italic="True" Font-Names="Bahnschrift SemiBold Condensed"
+                    Font-Size="Larger"></asp:Label>
+                <br />
+            </div>
+            <div>
+                <asp:Calendar ID="Calendar1" runat="server" BackColor="White" BorderColor="#3366CC" DayNameFormat="Shortest" OnDayRender="Calendar1_DayRender" Font-Names="Verdana"
+                    Font-Size="8pt" ForeColor="#003399" Height="200px" Width="299px" BorderWidth="1px" CellPadding="1">
+                    <DayHeaderStyle BackColor="#99CCCC" ForeColor="#336666" Height="1px" />
+                    <NextPrevStyle Font-Size="8pt" ForeColor="#CCCCFF" />
+                    <OtherMonthDayStyle ForeColor="#999999" />
+                    <SelectedDayStyle BackColor="#009999" ForeColor="#CCFF99" Font-Bold="True" />
+                    <SelectorStyle BackColor="#99CCCC" ForeColor="#336666" />
+                    <TitleStyle BackColor="#003399" Font-Bold="True" Font-Size="10pt" ForeColor="#CCCCFF" Height="25px" BorderColor="#3366CC" BorderWidth="1px" />
+                    <TodayDayStyle BackColor="#99CCCC" ForeColor="White" />
+                    <WeekendDayStyle BackColor="#CCCCFF" />
+                </asp:Calendar>
+            </div>
+            <br />
+            <br />
+            <div>
+                <asp:Label ID="Label1" runat="server" Text="Appointment Duration"></asp:Label><br />
+                <br />
+                <asp:DropDownList ID="AppointmentDurationList" runat="server"
+                    DataTextField="SlotDuration"
+                    DataValueField="SlotId"
+                    AppendDataBoundItems="true">
+                    <asp:ListItem Selected="False">---- Select Appointment Duration ----</asp:ListItem>
+                </asp:DropDownList>
+                <br />
+                <br />
+                <asp:Label ID="Label2" runat="server" Text="Appointment Time"></asp:Label><br />
+                <br />
+                <asp:DropDownList ID="AppointmentTimeList" runat="server"
+                    DataValueField="TimeId"
+                    DataTextField="Time"
+                    AppendDataBoundItems="true">
+                    <asp:ListItem Selected="False">---- Select Appointment Time ----</asp:ListItem>
+                </asp:DropDownList>
+                <br />
+                <br />
+                <asp:Button ID="Button1" runat="server" Text="Book Your Slot" OnClick="Button1_Click" BackColor="#00CCFF" BorderColor="#FFCCCC" />
+                <br />
+                <br />
+            </div>
+        </form>
+    </center>
+    <p>
+&nbsp;</p>
+</body>
+</html>
---
+++ Appointment Booking Project/SlotBooking.aspx.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace WebApplication1.slot_booking
+{
+    public partial class SlotBooking : System.Web.UI.Page
+    {
+        private SqlConnection conn;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["conStr"].ConnectionString;
+            conn = new SqlConnection(CS);
+            conn.Open();
+            if(!IsPostBack)
+            {
+                AppointmentDurationDropDown();
+                AppointmentTimeDropDown();
+            }
+        }
+        public void AppointmentDurationDropDown()
+        {
+            SqlCommand cmd = new SqlCommand("sp_getSlot", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataReader reader = cmd.ExecuteReader();
+            AppointmentDurationList.DataSource = reader;
+            AppointmentDurationList.DataBind();
+            reader.Close();
+            
+        }
+        public void AppointmentTimeDropDown()
+        {
+            SqlCommand cmd = new SqlCommand("sp_getDuration", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataReader reader = cmd.ExecuteReader();
+            AppointmentTimeList.DataSource = reader;
+            AppointmentTimeList.DataBind();
+            reader.Close();
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DateTime dates = Calendar1.SelectedDate.Date;
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO Appointment(AppointmentDate,SlotId,TimeId) VALUES(@ad,@sid,@tid)", conn);
+            //cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@ad", Calendar1.SelectedDate.Date);
+            cmd.Parameters.AddWithValue("@sid", AppointmentDurationList.SelectedItem.Text);
+
+            //if (Calendar1.SelectedDate.Date == DateTime.Today)
+            //{
+            //    SqlCommand cmd = new SqlCommand("InsertAppointment", conn);
+            //    cmd.CommandType = CommandType.StoredProcedure;
+            //    cmd.Parameters.AddWithValue("AppointmentDate", Calendar1.SelectedDate);
+            //    cmd.Parameters.AddWithValue("DurationHours",AppointmentDurationList.SelectedItem.Text);
+            //    cmd.Parameters.AddWithValue("AppointmentTime", AppointmentTimeList.SelectedItem.Text);
+            //    int Isbooked;
+            //    if (AppointmentTimeList.SelectedValue == AppointmentTimeList.SelectedValue)
+            //    {---
+            //        Isbooked = 1;
+            //    }
+            //    else
+            //    {
+            //        Isbooked = 0;
+            //    }
+            //    cmd.Parameters.AddWithValue("IsBooked", Isbooked);
+            //    cmd.ExecuteNonQuery();
+            //    Remove();
+            //}
+        }
+
+        public void Remove()
+        {
+            //SqlCommand cmd = new SqlCommand("RemoveAppointment", conn);
+            //cmd.CommandType = CommandType.StoredProcedure;
+            //int SelectedIndex = Convert.ToInt32(AppointmentTimeList.SelectedItem.Value);
+            //int AppointmentDuration = Convert.ToInt32(AppointmentDurationList.SelectedItem.Value);
+            //cmd.Parameters.AddWithValue("@SelectedIndex", SelectedIndex);
+            //cmd.Parameters.AddWithValue("@AppointmentDuration", AppointmentDuration);
+            //cmd.ExecuteNonQuery();
+            //SqlDataAdapter da = new SqlDataAdapter(cmd);
+            //DataTable dt = new DataTable();
+            //da.Fill(dt);
+            //AppointmentTimeList.DataBind();
+        }
+        //public void Remove()
+        //{
+        //    int selectedIndex = AppointmentTimeList.SelectedIndex;
+
+        //    if (selectedIndex != -1)
+        //    {
+        //        AppointmentTimeList.Items.RemoveAt(selectedIndex);
+        //        if (Convert.ToInt32(AppointmentDurationList.SelectedItem.Value) == 2 && selectedIndex < AppointmentTimeList.Items.Count)
+        //        {
+        //            AppointmentTimeList.Items.RemoveAt(selectedIndex);
+        //        }
+        //        AppointmentTimeList.DataBind();
+        //    }
+        //}
+        //public void remove()
+        //{
+        //    if (Convert.ToInt32(AppointmentDurationList.SelectedItem.Value) == 2)
+        //    {
+        //        for (int i = Convert.ToInt32(AppointmentDurationList.SelectedItem.Value); i <= 3; i++)
+        //        {
+        //            AppointmentTimeList.Items.Remove(AppointmentTimeList.SelectedItem);
+        //        }
+        //        AppointmentTimeList.DataBind();
+        //    }
+        //    else
+        //    {
+        //        AppointmentTimeList.Items.Remove(AppointmentTimeList.SelectedItem);
+        //        AppointmentTimeList.DataBind();
+        //    }
+        //}
+        //public void remove()
+        //{
+        //    if(Convert.ToInt32(AppointmentDurationList.SelectedItem.Value)==2)
+        //    {
+
+        //        AppointmentTimeList.Items.Remove(AppointmentTimeList.SelectedItem);
+        //        AppointmentTimeList.DataBind();
+        //    }
+        //    else
+        //    {
+        //        AppointmentTimeList.Items.Remove(AppointmentTimeList.SelectedItem);
+        //        AppointmentTimeList.DataBind();
+        //    }
+        //}
+
+
+        protected void Calendar1_DayRender(object sender, DayRenderEventArgs e)
+        {
+            if (e.Day.Date.CompareTo(DateTime.Today) < 0)
+            {
+                e.Day.IsSelectable = false;
+            }
+        }
+    }
+}
